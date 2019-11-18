019ccf0bae62125da59e1abb20aebbbe652751b2
Swell <austinphillips@Austins-MacBook-Pro.local>
2019-11-18 11:54:51 -0700

    add

--- main.js
+++ main.js
@@ -96,7 +96,7 @@ app.post('/api/swell', (request, response) => {
         sendInvite(res.data.id, token, locations, campaign_id, send_at, response)
     })
     .catch(err => {
-        if (err.response.data.error.email){
+        if (err.response.data.errors.email){
             console.log("Made it here.")
         } else {
             console.log("Wrong one")
