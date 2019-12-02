043c5cf6faa0849f8da66d7f771b8f2c95dd56c2
Swell <austinphillips@Austins-MacBook-Pro.local>
2019-12-02 13:08:40 -0700

    Fix bug

--- main.js
+++ main.js
@@ -139,16 +139,16 @@ getContact = (email, phone, token, locations, campaign_id, send_at, response) =>
 sendInvite = (contact_id, token, location_id, campaign_id, send_at, response) => {
     const url = process.env.INVITES_URL;
     const datetime = new Date();
-    const mst = moment.parseZone(datetime).utc(-7).format("YYYY-MM-DD H:m");
-    const formatted = moment.parseZone(datetime).utc(-7).format("YYYY-MM-DD" + " " + send_at);
-    const nextDay = moment.parseZone(formatted).add(1, 'd').utc(-7).format("YYYY-MM-DD H:m")
-    const final = formatted < mst ? formatted : nextDay;
+    const mst = moment.parseZone(datetime).utc(-7).format("YYYY-MM-DD H:mm");
+    const formatted = moment.parseZone(datetime).utc(-7).format("YYYY-MM-DD" + " " + "13:30");
+    const nextDay = moment.parseZone(formatted).add(1, 'd').utc(-7).format("YYYY-MM-DD H:mm");
+    const final = formatted > mst ? formatted : nextDay;
     const arr = {
         token,
         location_id,
         contact_id,
         campaign_id,
-        scheduled: true,
+        scheduled: formatted > mst ? nextDay : formatted,
         send_at: final
     };
     axios.post(url, arr, config)
