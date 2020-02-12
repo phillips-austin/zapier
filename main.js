21fa854fc509ccd4cf624975ff91da5c449fd8d0
Swell <austinphillips@Austins-MacBook-Pro.local>
2020-02-11 17:31:56 -0700

    add

--- main.js
+++ main.js
@@ -34,10 +34,7 @@ app.post('/api/test', (request, res) => {
     const formattedDate = moment(date).format("YYYY-MM-DD");
     const finalDate = formattedDate + ' ' + `${hourConverted}:${minute}`
     const now = new Date().getTime();
-
-    console.log()
-    console.log((hour+12))
-    console.log(hour+12)
+    console.log(now)
 
     res.sendStatus(200)
 })
