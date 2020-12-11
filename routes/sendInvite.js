2c442b261f59745c41df0f17c6f24ebd62a214c3
Austin <phillips.austin51@gmail.com>
2020-12-11 16:10:59 -0700

    format phone number

--- routes/sendInvite.js
+++ routes/sendInvite.js
@@ -14,14 +14,17 @@ router.post('/', (request, response) => {
     var {token, phone, name, email, locations, campaign_id, how, date, hour, ampm, minute} = request.body;
     email = email.split(',')[0];
     phone = phone.split(',')[0];
+    const phoneFormatted = phone.replace(/[^\d\+]/g,"");
     const url = process.env.CONTACTS_URL;
     const arr = {
         token,
         name,
         email,
-        phone,
+        phoneFormatted,
         locations: {id: locations}
     }
+
+    console.log(arr)
     // Create Contact
     axios.post(url, arr, config)
     .then(res => {
