0d59395df8eef57f8218036bf6c7dfb351a1b7e1
Austin Phillips <austinphillips@Austins-MacBook-Pro.local>
2021-02-02 21:25:54 -0700

    update schedule

--- functions/sendInvite.js
+++ functions/sendInvite.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const moment = require('moment');
 
 const config = {
     headers: {
@@ -57,7 +58,7 @@ async function sendInviteScheduled(contact_id, token, location_id, campaign_id, 
     }
 }
 
-scheduleInvite = (contact_id, token, location_id, campaign_id, how, date, hour, minute, ampm, response) => {
+scheduleInviteExample = (contact_id, token, location_id, campaign_id, how, date, hour, minute, ampm, response) => {
     const url = process.env.INVITES_URL;
     const dateConverted = new Date(date)
     const year = dateConverted.getFullYear()
@@ -162,6 +163,102 @@ scheduleInvite = (contact_id, token, location_id, campaign_id, how, date, hour, 
         //         }
         //     })
         // )
+    }
+}
+
+scheduleInvite = (contact_id, token, location_id, campaign_id, how, date, hour, minute, ampm, response) => {
+    const url = process.env.INVITES_URL;
+    const dateConverted = new Date(date)
+    const year = dateConverted.getFullYear()
+    const month = dateConverted.getMonth() < 9 ? `0${dateConverted.getMonth() + 1}` : dateConverted.getMonth() + 1;
+    const day = dateConverted.getUTCDate() < 10 ? `0${dateConverted.getUTCDate()}` : dateConverted.getUTCDate();
+    const hourConverted = () => {
+        if (hour === 12) {
+            return hour;
+        } else {
+            if (ampm === 'AM') {
+                if (hour > 9) {
+                    return hour;
+                } else {
+                    return `0${hour}`;
+                }
+            } else if (ampm === 'PM') {
+                return hour + 12;
+            }
+        }
+    }
+    var now = new Date().toLocaleString("en-US", {timeZone: "America/Denver"});
+    now = new Date(now)
+    const scheduleDate = `${year}-${month}-${day} ${hourConverted()}:${minute}`;
+
+    if (hour) {
+        const dateConverted = new Date(date)
+        const year = dateConverted.getFullYear()
+        const month = dateConverted.getMonth() < 9 ? `0${dateConverted.getMonth() + 1}` : dateConverted.getMonth() + 1;
+        const day = dateConverted.getUTCDate() < 10 ? `0${dateConverted.getUTCDate()}` : dateConverted.getUTCDate();
+        const hourConverted = () => {
+            if (hour === 12) {
+                return hour;
+            } else {
+                if (ampm === 'AM') {
+                    if (hour > 9) {
+                        return hour;
+                    } else {
+                        return `0${hour}`;
+                    }
+                } else if (ampm === 'PM') {
+                    return hour + 12;
+                }
+            }
+        }
+        const scheduleDate = `${year}-${month}-${day} ${hourConverted()}:${minute}`;
+        const arr = {
+            token,
+            location_id,
+            contact_id,
+            campaign_id,
+            scheduled: true,
+            send_at: scheduleDate
+        };
+
+        return (
+            axios.post(url, arr, config)
+            .then(res => {
+                return(res.data)
+            })
+            .catch(err => {
+                if (err.response.data.contact_id) {
+                    return({message: err.response.data.message})
+                } else {
+                    return({message: err.response.data.message})
+                }
+            })
+        )
+    } else {
+        const dateConverted = new Date(date)
+        const scheduleDate = moment(dateConverted).format("YYYY-MM-DD HH:mm");
+        const arr = {
+            token,
+            location_id,
+            contact_id,
+            campaign_id,
+            scheduled: true,
+            send_at: scheduleDate
+        };
+
+        return (
+            axios.post(url, arr, config)
+            .then(res => {
+                return(res.data)
+            })
+            .catch(err => {
+                if (err.response.data.contact_id) {
+                    return({message: err.response.data.message})
+                } else {
+                    return({message: err.response.data.message})
+                }
+            })
+        )
     }
 }
 
