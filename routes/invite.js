f861b6882732ebdb2dbd4e2627160424402af47e
Austin Phillips <austinphillips@Austins-MacBook-Pro.local>
2021-02-25 11:23:02 -0700

    update console log heroku

--- routes/invite.js
+++ routes/invite.js
@@ -108,7 +108,7 @@ function send(id, token, locations, campaign_id, how, date, hour, ampm, minute, 
                     return res.invite.message
                 })
                 .catch(err => {
-                    console.log("Line: 39")
+                    console.log(err)
                     return err
                 })
     } else {
