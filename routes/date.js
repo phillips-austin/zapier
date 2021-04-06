3185ede1d7c121b42ee0cdcf474a536c6145b7e6
phillips-austin <64037875+phillips-austin@users.noreply.github.com>
2021-04-06 10:58:06 -0600

    change to a post

--- routes/date.js
+++ routes/date.js
@@ -5,27 +5,25 @@ router.post('/', (request, response) => {
     var {date, operator, unit} = request.body;
     const incomingDate = new Date(date)
 
-    console.log(request)
-
-    // if(operator === 'Add') {
-    //     add(incomingDate, unit)
-    //     .then(res => {
-    //         console.log({Date: res})
-    //         response.json({date: res})
-    //     })
-    //     .catch(err => {
-    //         response.status(500).send({message: "oops something went wrong, please try again."})
-    //     })
-    // } else {
-    //     minus(incomingDate, unit)
-    //     .then(res => {
-    //         console.log({Date: res})
-    //         response.json({date: res})
-    //     })
-    //     .catch(err => {
-    //         response.status(500).send({message: "oops something went wrong, please try again."})
-    //     })
-    // }
+    if(operator === 'Add') {
+        add(incomingDate, unit)
+        .then(res => {
+            console.log({Date: res})
+            response.json({date: res})
+        })
+        .catch(err => {
+            response.status(500).send({message: "oops something went wrong, please try again."})
+        })
+    } else {
+        minus(incomingDate, unit)
+        .then(res => {
+            console.log({Date: res})
+            response.json({date: res})
+        })
+        .catch(err => {
+            response.status(500).send({message: "oops something went wrong, please try again."})
+        })
+    }
 
     async function add(incomingDate, unit) {
         const data = await new Date(incomingDate.setDate(incomingDate.getDate()+ unit))
