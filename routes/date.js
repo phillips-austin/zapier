ac894840b40ff1acfd454774bcd021a36b3e49ae
phillips-austin <64037875+phillips-austin@users.noreply.github.com>
2021-04-06 10:41:53 -0600

    add adync functions

--- routes/date.js
+++ routes/date.js
@@ -4,17 +4,34 @@ const router = require('express').Router();
 router.get('/', (request, response) => {
     var {date, operator, unit} = request.body;
     const incomingDate = new Date(date)
-    operator = operator === "Add" ? add(incomingDate, unit) : minus(incomingDate, unit);
 
-    function add(date, unit) {
-        return new Date(date.setDate(date.getDate()+ unit))
+    if(operator === 'Add') {
+        add(incomingDate, unit)
+        .then(res => {
+            response.json({date: res})
+        })
+        .catch(err => {
+            response.status(500).send({message: "oops something went wrong, please try again."})
+        })
+    } else {
+        minus(incomingDate, unit)
+        .then(res => {
+            response.json({date: res})
+        })
+        .catch(err => {
+            response.status(500).send({message: "oops something went wrong, please try again."})
+        })
     }
 
-    function minus(date, unit) {
-        return new Date(date.setDate(date.getDate()- unit))
+    async function add(date, unit) {
+        const data = await new Date(date.setDate(date.getDate()+ unit))
+        return data
     }
 
-    response.json({date: operator})
+    async function minus(date, unit) {
+        const data = await new Date(date.setDate(date.getDate()- unit))
+        return data
+    }
 });
 
 module.exports = router;
